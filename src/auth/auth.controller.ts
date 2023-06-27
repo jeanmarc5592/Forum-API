7be92f0fa8fb58d3aaa5f8cadd5e683dc73a2d57
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-06-27 17:33:56 +0200

    Setup login method in Controller and Service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Req } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  // TODO: Type "req" correctly
+  login(@Req() req: { user: any }) {
+    return this.authService.login(req.user);
+  }
+
+  // TODO: Implement logout
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/users/entities/users.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  login(user: User) {
+    // TODO: Sign and return new JWT as "access_token"
+    return {
+      access_token: 'TOKEN',
+    };
+  }
+}
