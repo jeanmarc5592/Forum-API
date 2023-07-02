b4b3a20dc44b10d25b0251f2e45616495bdcc576
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-07-02 18:17:40 +0200

    Add Unit Tests for RefreshTokenStrategy

---
+++ src/auth/strategies/refresh-token.strategy.spec.ts
@@ -0,0 +1,43 @@
+import { ConfigService } from '@nestjs/config';
+import { Strategy } from 'passport-jwt';
+import { RefreshTokenStrategy } from './refresh-token.strategy';
+import { Request } from 'express';
+import { JwtPayload } from '../auth.types';
+
+class MockConfigService extends ConfigService {
+  get(key: string) {
+    if (key === 'JWT_REFRESH_SECRET') {
+      return 'secret';
+    }
+    return undefined;
+  }
+}
+
+describe('RefreshTokenStrategy', () => {
+  let strategy: RefreshTokenStrategy;
+
+  beforeEach(() => {
+    const mockConfigService = new MockConfigService();
+    strategy = new RefreshTokenStrategy(mockConfigService);
+  });
+
+  it('should create an instance of AccessTokenStrategy', () => {
+    expect(strategy).toBeInstanceOf(RefreshTokenStrategy);
+    expect(strategy).toBeInstanceOf(Strategy);
+  });
+
+  it('should validate the request and payload', () => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const req = { get: jest.fn(() => 'Bearer token') } as Request;
+    const payload = { sub: 'user123' } as JwtPayload;
+
+    const validatedData = strategy.validate(req, payload);
+
+    expect(req.get).toHaveBeenCalledWith('Authorization');
+    expect(validatedData).toEqual({
+      id: 'user123',
+      refreshToken: 'token',
+    });
+  });
+});
