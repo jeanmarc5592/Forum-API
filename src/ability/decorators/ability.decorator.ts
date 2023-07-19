209f845d03ad10afff54e812382c878c25b1c15a
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-07-19 15:46:46 +0200

    Delete Ability Decorator

--- src/ability/decorators/ability.decorator.ts
+++
@@ -1,8 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { RequiredRule } from '../ability.types';
-
-export const CHECK_ABILITY = 'check_ability';
-
-export const CheckAbilities = (...requirements: RequiredRule[]) => {
-  return SetMetadata(CHECK_ABILITY, requirements);
-};
