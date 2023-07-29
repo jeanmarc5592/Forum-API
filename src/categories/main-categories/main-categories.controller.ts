01a8a392e6253cf957256ad1afb9d048fa1aadc1
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-07-29 13:02:53 +0200

    Add AccessTokenGuards

--- src/categories/main-categories/main-categories.controller.ts
+++ src/categories/main-categories/main-categories.controller.ts
@@ -6,10 +6,12 @@ import {
   Post,
   Delete,
   Body,
+  UseGuards,
 } from '@nestjs/common';
 import { MainCategoriesService } from './main-categories.service';
 import { UpdateMainCategoryDTO } from './dtos/update-main-category.dto';
 import { CreateMainCategoryDTO } from './dtos/create-main-category.dto';
+import { AccessTokenGuard } from 'src/auth/guards/access-token.guard';
 
 @Controller('main-categories')
 export class MainCategoriesController {
@@ -25,16 +27,19 @@ export class MainCategoriesController {
     return this.mainCategoriesService.getById(id);
   }
 
+  @UseGuards(AccessTokenGuard)
   @Patch('/:id')
   update(@Param('id') id: string, @Body() body: UpdateMainCategoryDTO) {
     return this.mainCategoriesService.update(body, id);
   }
 
+  @UseGuards(AccessTokenGuard)
   @Delete('/:id')
   delete(@Param('id') id: string) {
     return this.mainCategoriesService.delete(id);
   }
 
+  @UseGuards(AccessTokenGuard)
   @Post()
   create(@Body() body: CreateMainCategoryDTO) {
     return this.mainCategoriesService.create(body);
