377ed3dbc1673a357145a44a40f82f08b2c055ed
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-07-29 12:00:44 +0200

    Hook Up Methods

--- src/categories/main-categories/main-categories.controller.ts
+++ src/categories/main-categories/main-categories.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Get, Patch, Post, Delete } from '@nestjs/common';
+import { MainCategoriesService } from './main-categories.service';
 
 @Controller('main-categories')
-export class MainCategoriesController {}
+export class MainCategoriesController {
+  constructor(private readonly mainCategoriesService: MainCategoriesService) {}
+
+  @Get()
+  getAll() {
+    return this.mainCategoriesService.getAll();
+  }
+
+  @Get('/:id')
+  getById(@Param('id') id: string) {
+    return this.mainCategoriesService.getById(id);
+  }
+
+  @Patch('/:id')
+  update(@Param('id') id: string) {
+    return this.mainCategoriesService.update(id);
+  }
+
+  @Post()
+  add() {
+    return this.mainCategoriesService.add();
+  }
+
+  @Delete('/:id')
+  delete(@Param('id') id: string) {
+    return this.mainCategoriesService.delete(id);
+  }
+}
--- src/categories/main-categories/main-categories.service.ts
+++ src/categories/main-categories/main-categories.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class MainCategoriesService {}
+export class MainCategoriesService {
+  getAll() {
+    return 'GET ALL';
+  }
+
+  getById(id: string) {
+    return 'GET WITH ID ' + id;
+  }
+
+  update(id: string) {
+    return 'UPDATE WITH ID ' + id;
+  }
+
+  add() {
+    return 'ADD A NEW ONE';
+  }
+
+  delete(id: string) {
+    return 'DELETE WITH ID ' + id;
+  }
+}
