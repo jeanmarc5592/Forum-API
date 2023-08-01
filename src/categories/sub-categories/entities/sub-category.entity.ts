4d1639b1ed718a29a6b32d5a2a5a3d5d8b299588
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-08-01 15:00:46 +0200

    Create Sub Category Entity

--- src/categories/sub-categories/entities/sub-category.entity.ts
+++ src/categories/sub-categories/entities/sub-category.entity.ts
@@ -1,1 +1,41 @@
-export class SubCategory {}
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  BeforeInsert,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Unique,
+  ManyToOne,
+} from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+import { MainCategory } from 'src/categories/main-categories/entities/main-category.entity';
+
+@Entity()
+export class SubCategory {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  @Unique(['name'])
+  name: string;
+
+  @Column()
+  description: string;
+
+  @ManyToOne(() => MainCategory, (mainCategory) => mainCategory.subCategories)
+  mainCategory: MainCategory;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updated_at: Date;
+
+  @BeforeInsert()
+  generateId() {
+    if (!this.id) {
+      this.id = uuidv4();
+    }
+  }
+}
--- src/categories/sub-categories/sub-categories.module.ts
+++ src/categories/sub-categories/sub-categories.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SubCategoriesService } from './sub-categories.service';
 import { SubCategoriesController } from './sub-categories.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SubCategory } from './entities/sub-category.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([SubCategory])],
   controllers: [SubCategoriesController],
-  providers: [SubCategoriesService]
+  providers: [SubCategoriesService],
 })
 export class SubCategoriesModule {}
