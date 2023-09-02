ae2abd132c9b12c70aef3aa83cc20621574676d0
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-09-02 10:42:49 +0200

    Add unit tests to SubCategoriesService

--- test/unit/categories/sub-categories/sub-categories.service.spec.ts
+++ test/unit/categories/sub-categories/sub-categories.service.spec.ts
@@ -1,14 +1,17 @@
-import { NotFoundException } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { MockType } from '@/app.types';
+import { Roles } from '@auth/auth.types';
 import { MainCategoriesService } from '@categories/main-categories/main-categories.service';
 import { CreateSubCategoryDto } from '@categories/sub-categories/dtos/create-sub-category.dto';
 import { UpdateSubCategoryDto } from '@categories/sub-categories/dtos/update-sub-category.dto';
 import { SubCategory } from '@categories/sub-categories/entities/sub-category.entity';
 import { SubCategoriesService } from '@categories/sub-categories/sub-categories.service';
+import { User } from '@users/entities/user.entity';
+import { UsersService } from '@users/users.service';
 
 import {
   MockSubCategoriesRepository,
@@ -24,6 +27,7 @@ describe('SubCategoriesService', () => {
   let service: SubCategoriesService;
   let mainCatService: MainCategoriesService;
   let repositoryMock: MockType<Repository<SubCategory>>;
+  let usersService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -38,6 +42,7 @@ describe('SubCategoriesService', () => {
     service = module.get<SubCategoriesService>(SubCategoriesService);
     mainCatService = module.get<MainCategoriesService>(MainCategoriesService);
     repositoryMock = module.get(getRepositoryToken(SubCategory));
+    usersService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
@@ -133,6 +138,177 @@ describe('SubCategoriesService', () => {
       await expect(service.getTopics('12334')).rejects.toThrow(
         NotFoundException,
       );
+    });
+  });
+
+  describe('getModerators', () => {
+    it('should return the moderators of the given sub category', async () => {
+      repositoryMock.createQueryBuilder?.mockReturnValue({
+        leftJoinAndSelect: jest.fn().mockReturnThis(),
+        where: jest.fn().mockReturnThis(),
+        getOne: jest.fn().mockResolvedValue(mockSubCat),
+      });
+
+      const moderators = await service.getModerators(mockSubCat.id);
+
+      expect(moderators).toEqual(mockSubCat.moderators);
+    });
+
+    it('should return an empty list for moderators', async () => {
+      Object.assign(mockSubCat, { moderators: [] });
+
+      repositoryMock.createQueryBuilder?.mockReturnValue({
+        leftJoinAndSelect: jest.fn().mockReturnThis(),
+        where: jest.fn().mockReturnThis(),
+        getOne: jest.fn().mockResolvedValue(mockSubCat),
+      });
+
+      const moderators = await service.getModerators(mockSubCat.id);
+
+      expect(moderators).toEqual([]);
+    });
+
+    it('should throw a NotFoundExpception if sub category was not found', async () => {
+      repositoryMock.createQueryBuilder?.mockReturnValue({
+        leftJoinAndSelect: jest.fn().mockReturnThis(),
+        where: jest.fn().mockReturnThis(),
+        getOne: jest.fn().mockResolvedValue(null),
+      });
+
+      await expect(service.getModerators('12334')).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+  });
+
+  describe('addModerator', () => {
+    it('should add a new moderator', async () => {
+      const newModerator = {
+        id: '123',
+        name: 'New Moderator',
+        role: Roles.MODERATOR,
+      } as User;
+      jest.spyOn(usersService, 'getById').mockResolvedValue(newModerator);
+
+      repositoryMock.createQueryBuilder?.mockReturnValue({
+        leftJoinAndSelect: jest.fn().mockReturnThis(),
+        where: jest.fn().mockReturnThis(),
+        getOne: jest.fn().mockResolvedValue(mockSubCat),
+      });
+
+      const updatedSubCat = {
+        ...mockSubCat,
+        moderators: mockSubCat.moderators.concat(newModerator),
+      };
+      repositoryMock.save?.mockReturnValue(updatedSubCat);
+
+      const subCat = await service.addModerator(mockSubCat.id, newModerator.id);
+
+      expect(subCat.moderators).toContain(newModerator);
+    });
+
+    it('should throw BadRequestException if user has not a moderator role', async () => {
+      const newModerator = {
+        id: '123',
+        name: 'New Moderator',
+        role: Roles.USER,
+      } as User;
+      jest.spyOn(usersService, 'getById').mockResolvedValue(newModerator);
+
+      await expect(
+        service.addModerator(mockSubCat.id, newModerator.id),
+      ).rejects.toThrow(BadRequestException);
+    });
+
+    it('should throw NotFoundException if the sub category was not found', async () => {
+      const newModerator = {
+        id: '123',
+        name: 'New Moderator',
+        role: Roles.MODERATOR,
+      } as User;
+      jest.spyOn(usersService, 'getById').mockResolvedValue(newModerator);
+
+      repositoryMock.createQueryBuilder?.mockReturnValue({
+        leftJoinAndSelect: jest.fn().mockReturnThis(),
+        where: jest.fn().mockReturnThis(),
+        getOne: jest.fn().mockResolvedValue(null),
+      });
+
+      await expect(
+        service.addModerator('123456', newModerator.id),
+      ).rejects.toThrow(NotFoundException);
+    });
+
+    it('should throw BadRequestException if user is already a moderator', async () => {
+      const newModerator = {
+        id: '123',
+        name: 'New Moderator',
+        role: Roles.MODERATOR,
+      } as User;
+      jest.spyOn(usersService, 'getById').mockResolvedValue(newModerator);
+
+      Object.assign(mockSubCat, {
+        moderators: mockSubCat.moderators.concat(newModerator),
+      });
+
+      repositoryMock.createQueryBuilder?.mockReturnValue({
+        leftJoinAndSelect: jest.fn().mockReturnThis(),
+        where: jest.fn().mockReturnThis(),
+        getOne: jest.fn().mockResolvedValue(mockSubCat),
+      });
+
+      await expect(
+        service.addModerator(mockSubCat.id, newModerator.id),
+      ).rejects.toThrow(BadRequestException);
+    });
+  });
+
+  describe('deleteModerator', () => {
+    it('should delete a moderator', async () => {
+      repositoryMock.createQueryBuilder?.mockReturnValue({
+        leftJoinAndSelect: jest.fn().mockReturnThis(),
+        where: jest.fn().mockReturnThis(),
+        getOne: jest.fn().mockResolvedValue(mockSubCat),
+      });
+
+      const updatedSubCat = {
+        ...mockSubCat,
+        moderators: [],
+      };
+      repositoryMock.save?.mockReturnValue(updatedSubCat);
+
+      const moderatorToDelete = mockSubCat.moderators[0];
+
+      const subCat = await service.deleteModerator(
+        mockSubCat.id,
+        moderatorToDelete.id,
+      );
+
+      expect(subCat.moderators).not.toContain(moderatorToDelete);
+    });
+
+    it('should throw a NotFoundException if sub category was not found', async () => {
+      repositoryMock.createQueryBuilder?.mockReturnValue({
+        leftJoinAndSelect: jest.fn().mockReturnThis(),
+        where: jest.fn().mockReturnThis(),
+        getOne: jest.fn().mockResolvedValue(null),
+      });
+
+      await expect(
+        service.deleteModerator('123456', '42232342'),
+      ).rejects.toThrow(NotFoundException);
+    });
+
+    it('should throw a BadRequestException if user is not a moderator', async () => {
+      repositoryMock.createQueryBuilder?.mockReturnValue({
+        leftJoinAndSelect: jest.fn().mockReturnThis(),
+        where: jest.fn().mockReturnThis(),
+        getOne: jest.fn().mockResolvedValue(mockSubCat),
+      });
+
+      await expect(
+        service.deleteModerator('123456', '42232342'),
+      ).rejects.toThrow(BadRequestException);
     });
   });
 
