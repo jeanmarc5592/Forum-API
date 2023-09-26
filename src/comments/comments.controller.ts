e55ec60bb46c9c7feaf2a4ca10df4a1fe2231ef7
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-09-26 09:55:30 +0200

    Scaffold controller and service methods

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Body,
+  Req,
+} from '@nestjs/common';
+
+import { RequestUser } from '@/auth/auth.types';
+
+import { CommentsService } from './comments.service';
 
 @Controller('comments')
-export class CommentsController {}
+export class CommentsController {
+  constructor(private readonly commentsService: CommentsService) {}
+
+  @Get('/:id')
+  getById(@Param('id') id: string) {
+    return this.commentsService.getById(id);
+  }
+
+  @Patch('/:id')
+  update(
+    @Param('id') id: string,
+    @Body() body: object,
+    @Req() req: { user: RequestUser },
+  ) {
+    return this.commentsService.update(body, id);
+  }
+
+  @Delete('/:id')
+  delete(@Param('id') id: string, @Req() req: { user: RequestUser }) {
+    return this.commentsService.delete(id);
+  }
+
+  @Post()
+  create(@Body() body: object, @Req() req: { user: RequestUser }) {
+    return this.commentsService.create(body, req.user.id);
+  }
+}
--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CommentsService {}
+export class CommentsService {
+  getById(id: string) {
+    return 'get by id';
+  }
+
+  update(commentDTO: object, id: string) {
+    return 'update';
+  }
+
+  delete(id: string) {
+    return 'delete';
+  }
+
+  create(commentDTO: object, userId: string) {
+    return 'create';
+  }
+}
