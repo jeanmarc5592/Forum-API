77aa64a17c82e274bcab0de8daafdd3c4b503f9a
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-09-29 12:59:33 +0200

    Add new unit test for UsersUtils

--- test/unit/users/users.utils.spec.ts
+++ test/unit/users/users.utils.spec.ts
@@ -3,6 +3,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersUtils } from '@users/users.utils';
 
 import { mockUser } from './fixtures/users.fixtures';
+import { mockComment } from '../comments/fixtures/comments.fixtures';
 
 describe('UsersUtils', () => {
   let usersUtils: UsersUtils;
@@ -19,9 +20,17 @@ describe('UsersUtils', () => {
     expect(usersUtils).toBeDefined();
   });
 
-  it('should extract the sub categories of the given main category', () => {
+  it('should extract the topics of the given user', () => {
     const result = usersUtils.getTopics(mockUser);
 
     expect(result).toEqual(mockUser.topics);
+  });
+
+  it('should extract the comments of given user', () => {
+    mockUser.comments = [mockComment];
+
+    const result = usersUtils.getComments(mockUser);
+
+    expect(result).toEqual(mockUser.comments);
   });
 });
