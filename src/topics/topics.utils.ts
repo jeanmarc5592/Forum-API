f7d28ea3a48dffbcb4b3317c8637476ff6755c9b
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-10-13 20:17:09 +0200

    Remove console.log

--- src/topics/topics.utils.ts
+++ src/topics/topics.utils.ts
@@ -25,7 +25,6 @@ export class TopicsUtils {
   }
 
   transformComments(comments: Comment[]): TransformedComment[] {
-    console.log(comments);
     return comments.map((comment) => {
       return {
         id: comment.id,
