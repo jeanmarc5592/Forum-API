52851dd900463ada3221cf666cfc86de39ca48aa
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-10-17 16:59:04 +0200

    Extract refreshToken from request

--- src/auth/strategies/refresh-token.strategy.ts
+++ src/auth/strategies/refresh-token.strategy.ts
@@ -2,25 +2,35 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { Request } from 'express';
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Strategy } from 'passport-jwt';
 
-import { JwtPayload } from '../auth.types';
+import { HttpUtils } from '@/utils/http.utils';
+
+import { JwtPayload, Tokens } from '../auth.types';
 
 @Injectable()
 export class RefreshTokenStrategy extends PassportStrategy(
   Strategy,
   'jwt-refresh',
 ) {
-  constructor(protected readonly configService: ConfigService) {
+  constructor(
+    protected readonly configService: ConfigService,
+    protected readonly httpUtils: HttpUtils,
+  ) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: (req: Request) => {
+        return httpUtils.extractCookieFromRequest(req, Tokens.REFRESH_TOKEN);
+      },
       secretOrKey: configService.get<string>('jwt.refresh.secret'),
       passReqToCallback: true,
     });
   }
 
   validate(req: Request, payload: JwtPayload) {
-    const refreshToken = req.get('Authorization')?.replace('Bearer', '').trim();
+    const refreshToken = this.httpUtils.extractCookieFromRequest(
+      req,
+      Tokens.REFRESH_TOKEN,
+    );
 
     return { id: payload.sub, refreshToken };
   }
