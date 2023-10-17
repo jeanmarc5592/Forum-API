8c51b59978bf17aea833a852c86232e42eda2415
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-10-17 16:25:45 +0200

    Add cookieParser

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 
 import { AppModule } from './app.module';
 
@@ -10,6 +11,8 @@ async function bootstrap() {
 
   const configService = app.get<ConfigService>(ConfigService);
   const port = configService.get('port');
+
+  app.use(cookieParser());
 
   await app.listen(port);
 }
